577bb4106fd547eacccdfe3e720e69eee5306c17
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2023-01-30 10:04:11 +0100

    ✨feature: Add basic svg components

---
+++ src/data/background/dataIconBackground.ts
@@ -0,0 +1,10 @@
+export const dataIconBackground = [
+    { top: '8vh', strenth: 0.9, left: '4vw' },
+    { top: '16vh', strenth: 0.15, left: '8vw' },
+    { top: '47vh', strenth: 0.65, left: '8vw' },
+    { top: '55vh', strenth: 0.85, left: '4vw' },
+    { top: '8vh', strenth: 0.9, right: '4vw' },
+    { top: '16vh', strenth: 0.15, right: '8vw' },
+    { top: '47vh', strenth: 0.65, right: '8vw' },
+    { top: '55vh', strenth: 0.85, right: '4vw' },
+]
