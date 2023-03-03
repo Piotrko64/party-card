7e1bf08941a867e2b4104352b523080808aaaf9f
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2023-03-03 22:54:41 +0100

    ✨feature: End creating wish wall form

--- src/components/generatorPage/form/sections/wishesFormSection/formElements/wishWall/WishWallForm.tsx
+++ src/components/generatorPage/form/sections/wishesFormSection/formElements/wishWall/WishWallForm.tsx
@@ -1,5 +1,40 @@
+import { fontsToChoice } from "data/fonts/fontsToChoice";
 import { WallWishType } from "types/stores/WishesSectionStore";
+import cx from "classnames";
+import { OrdinaryColors } from "ui/form/inputColorSection/ordinaryColors/OrdinaryColors";
+import { ListInputElement } from "ui/form/listInputElements/ListInputElements";
+import classes from "./wishWall.module.scss";
+import { useWishesSectionStore } from "stores/WishesSectionStore/WishesSectionStore";
+import { NamesFont } from "types/typesForStyles/NamesFont";
 
-export function WishWallForm(props: WallWishType) {
-    return <h3> Ściana życzeń</h3>;
+export function WishWallForm({ id: idElement, font, color, texts }: WallWishType) {
+    const { changeWishValue } = useWishesSectionStore();
+
+    function changeColorText(color: string) {
+        changeWishValue("color", color, idElement);
+    }
+
+    function changeFont(newFont: NamesFont) {
+        changeWishValue("font", newFont, idElement);
+    }
+
+    return (
+        <>
+            {" "}
+            <h3> Ściana życzeń</h3>
+            <div className={classes.fonts}>
+                {fontsToChoice.map(({ fullName, name }) => (
+                    <div
+                        className={cx(classes.font, font === name && classes.thisFont)}
+                        style={{ fontFamily: fullName }}
+                        onClick={() => changeFont(name)}
+                    >
+                        {name}
+                    </div>
+                ))}
+            </div>
+            <OrdinaryColors changeColor={changeColorText} selectedColor={color} />
+            <ListInputElement idElement={idElement} texts={texts} />
+        </>
+    );
 }
