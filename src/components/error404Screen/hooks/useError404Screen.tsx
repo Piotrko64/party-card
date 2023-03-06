6ec2a50db4d75e345690fab1f405095ad6cf9c97
Piotr Kołodziejczyk <77500425+Piotrko64@users.noreply.github.com>
2023-03-06 16:59:04 +0100

    Add files via upload

---
+++ src/components/error404Screen/hooks/useError404Screen.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+
+const TIME_TO_REDIRECT = 7500;
+
+export function useError404Screen() {
+    const navigate = useNavigate();
+    const { t } = useTranslation("page404");
+
+    useEffect(() => {
+        let timer = setTimeout(() => {
+            navigate("/");
+        }, TIME_TO_REDIRECT);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, []);
+
+    return { t };
+}
