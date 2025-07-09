f258370dfa62ee99bc175425c95a1d8ec4571509
Piotrko64 <PiotrPc77@gmail.com>
2025-07-09 17:45:26 +0200

    ADD: next footer link

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,31 +3,32 @@ import classes from "./footer.module.scss";
 import { useTranslation } from "react-i18next";
 
 export function Footer() {
-    const { t, i18n } = useTranslation("footer");
+  const { t, i18n } = useTranslation("footer");
 
-    function toggleLanguage() {
-        i18n.changeLanguage(i18n.language.includes("pl") ? "en" : "pl-PL");
-    }
+  function toggleLanguage() {
+    i18n.changeLanguage(i18n.language.includes("pl") ? "en" : "pl-PL");
+  }
 
-    return (
-        <footer className={classes.footer}>
-            <div className={classes.party}>PARTY CARD </div>
-            <div className={classes.links}>
-                <Link to="/"> {t("goToMain")}</Link>
-                <Link to="/generateCard"> {t("goToGenerator")}</Link>
-                <Link to="/examples/2"> {t("goToExamples")}</Link>
+  return (
+    <footer className={classes.footer}>
+      <div className={classes.party}>PARTY CARD </div>
+      <div className={classes.links}>
+        <Link to="/"> {t("goToMain")}</Link>
+        <Link to="/generateCard"> {t("goToGenerator")}</Link>
+        <Link to="/examples/2"> {t("goToExamples")}</Link>
+        <Link to="/aiGenerate"> {t("goToAI")}</Link>
 
-                <div onClick={toggleLanguage}> Przełącz język / Switch language</div>
-            </div>
-            <div className={classes.linkToPortfolio}>
-                <p>
-                    {t("realization")} <br />
-                    <a href="http://piotrkolodziejczyk.vercel.app" target="_blank">
-                        Piotr Kołodziejczyk
-                    </a>
-                </p>
-                <img src="/logoPK.png" alt="logo" />
-            </div>
-        </footer>
-    );
+        <div onClick={toggleLanguage}> Przełącz język / Switch language</div>
+      </div>
+      <div className={classes.linkToPortfolio}>
+        <p>
+          {t("realization")} <br />
+          <a href="http://piotrkolodziejczyk.vercel.app" target="_blank">
+            Piotr Kołodziejczyk
+          </a>
+        </p>
+        <img src="/logoPK.png" alt="logo" />
+      </div>
+    </footer>
+  );
 }
